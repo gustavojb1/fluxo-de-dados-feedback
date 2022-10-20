6c5942b25327454003aa3482cb07cf84107c5cc4
gustavojb1 <gustavojunqueirab@gmail.com>
2022-10-20 17:43:39 -0300

    finished

--- src/components/FormularioLogin/FormularioLogin.js
+++ src/components/FormularioLogin/FormularioLogin.js
@@ -7,20 +7,50 @@ import {
   StyledLabel,
 } from "../FormularioPostagem/styled";
 
+
 const FormularioLogin = (props) => {
+
+
   const login = () => {
+    
+
     props.setPageFlow(2);
+
+    const objetoHeader={
+      name:props.nome,
+      imagem:props.imagem
+    };
+
+    props.setObjetoHeader(objetoHeader)
   };
+
+  const onChangeImagem = (event) => {
+    // console.log("onchange",event.target.value);
+    props.setImagem(event.target.value);
+  };
+  const onChangeNome = (event) => {
+    // console.log("descricao", event.target.value);
+    props.setNome(event.target.value);
+  };
+
+
+  
+
+
   return (
     <FormContainer>
       <Form>
         <StyledLabel>
           Nome:
-          <Input type={"text"} />
+          <Input type={"text"} 
+          onChange={onChangeNome} value={props.nome}
+          />
         </StyledLabel>
         <StyledLabel>
           Foto de Perfil
-          <Input type={"text"} />
+          <Input type={"text"} 
+          onChange={onChangeImagem} value={props.imagem}
+          />
         </StyledLabel>
         <SendButton onClick={login}>Fazer Login</SendButton>
       </Form>
